46306f8251a9f0ca34c6e2675934b0ff4b593311
mark wang <834031172@qq.com>
2024-09-07 17:12:44 +0800

    init

---
+++ src/main/java/com/flarej/flarej/FlarejApplication.java
@@ -0,0 +1,13 @@
+package com.flarej.flarej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlarejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlarejApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flarej/flarej/FlarejApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flarej.flarej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlarejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
